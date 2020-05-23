05b773f369e297b22b99557551150b6b298aab02
Hugo Romano <hugoromano@gmail.com>
2020-05-23 18:35:46 +0100

    minor change

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
  * @param {number} lenght - number of characters of generated ID
  */
 
-module.exports = lenght => {
+const makeid = (lenght) => {
   let text = "";
   const possible = "ABCDEFGHIJKLMNPQRSTUVWXYZ0123456789";
   for (let i = 0; i < lenght; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   return text;
+};
+
+module.exports = {
+  makeid,
 };
