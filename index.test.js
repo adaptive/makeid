ac70e29c8aa7ab88026eebe58a813ea75731c078
Hugo Romano <hugoromano@gmail.com>
2020-05-29 13:10:07 +0100

    change on tests

--- index.test.js
+++ index.test.js
@@ -1,8 +1,12 @@
 const { makeid } = require("./index");
 
+test("generate an ID with default lenght", () => {
+  expect(makeid()).toHaveLength(6);
+});
+
 test("generate an ID with lenght of 1", () => {
-    expect(makeid(1)).toHaveLength(1);
-  });
+  expect(makeid(1)).toHaveLength(1);
+});
 
 test("generate an ID with lenght of 3", () => {
   expect(makeid(3)).toHaveLength(3);
@@ -10,10 +14,6 @@ test("generate an ID with lenght of 3", () => {
 
 test("generate an ID with lenght of 6", () => {
   expect(makeid(6)).toHaveLength(6);
-});
-
-test("generate an ID with lenght of 6", () => {
-  expect(makeid()).toHaveLength(6);
 });
 
 test("generate an ID with lenght of 32", () => {
@@ -28,6 +28,10 @@ test("generate an ID with lenght of 1000", () => {
   expect(makeid(1000)).toHaveLength(1000);
 });
 
-test("generate an ID with lenght of 1000", () => {
-    expect(makeid(10000)).toHaveLength(10000);
-  });
+test("generate an ID with lenght of 10000", () => {
+  expect(makeid(10000)).toHaveLength(10000);
+});
+
+test("generate an ID with lenght of 100000", () => {
+  expect(makeid(100000)).toHaveLength(100000);
+});
